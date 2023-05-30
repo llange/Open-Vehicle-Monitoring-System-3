5f2b0d21cbc5a7189d15497776e2e7f3252afa4e
Ludovic LANGE <llange@users.noreply.github.com>
2023-05-30 22:07:57 +0200

    MG7: ovms_netconns / ovms_nethttp (untested)

--- vehicle/OVMS.V3/components/ovms_netlib/src/ovms_netconns.cpp
+++ vehicle/OVMS.V3/components/ovms_netlib/src/ovms_netconns.cpp
@@ -38,9 +38,17 @@ static const char *TAG = "ovms-net";
 // OvmsMongooseWrapper
 ////////////////////////////////////////////////////////////////////////////////
 
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+static void OvmsMongooseWrapperCallback(struct mg_connection *nc, int ev, void *ev_data, void *fn_data)
+#else /* MG_VERSION_NUMBER */
 static void OvmsMongooseWrapperCallback(struct mg_connection *nc, int ev, void *ev_data)
+#endif /* MG_VERSION_NUMBER */
   {
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+  OvmsMongooseWrapper* me = (OvmsMongooseWrapper*)nc->fn_data;
+#else /* MG_VERSION_NUMBER */
   OvmsMongooseWrapper* me = (OvmsMongooseWrapper*)nc->user_data;
+#endif /* MG_VERSION_NUMBER */
 
   if (me != NULL) me->Mongoose(nc, ev, ev_data);
   }
@@ -72,8 +80,13 @@ OvmsNetTcpClient::~OvmsNetTcpClient()
   {
   if (m_mgconn)
     {
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+    m_mgconn->fn_data = NULL;
+    m_mgconn->is_closing = 1;         // Tell mongoose to close this connection
+#else /* MG_VERSION_NUMBER */
     m_mgconn->user_data = NULL;
     m_mgconn->flags |= MG_F_CLOSE_IMMEDIATELY;
+#endif /* MG_VERSION_NUMBER */
     m_mgconn = NULL;
     m_netstate = NetConnIdle;
     }
@@ -83,6 +96,50 @@ void OvmsNetTcpClient::Mongoose(struct mg_connection *nc, int ev, void *ev_data)
   {
   switch (ev)
     {
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+    case MG_EV_CONNECT:  // Connection established       NULL
+      {
+      // Successful connection
+      ESP_LOGD(TAG, "OvmsNetTcpClient Connection successful");
+      m_netstate = NetConnConnected;
+      Connected();
+      }
+      break;
+    case MG_EV_OPEN:
+      {
+      // Connection created. Store connect expiration time in c->data
+      if (m_timeout > 0.0)
+      {
+        *(uint64_t *) nc->data = (uint64_t)(mg_millis() + m_timeout);
+      } else {
+        *(uint64_t *) nc->data = NULL;
+      }
+      break;
+      }
+    case MG_EV_POLL:
+      {
+      if (*(uint64_t *) nc->data != NULL)
+      {
+        if ((mg_millis() > *(uint64_t *) nc->data) &&
+            (nc->is_connecting || nc->is_resolving))
+          {
+          ESP_LOGD(TAG, "OvmsNetTcpClient Connection timeout");
+          m_mgconn->fn_data = NULL;
+          mg_error(nc, "Connection timeout"); // will request a connection closure and fire the MG_EV_ERROR event.
+          }
+      }
+      break;
+      }
+    case MG_EV_ERROR:
+      {
+      char * err = (char *) ev_data;
+      ESP_LOGD(TAG, "OvmsNetTcpClient Connection failed: %s", err);
+      m_netstate = NetConnFailed;
+      m_mgconn = NULL;
+      ConnectionFailed();
+      break;
+      }
+#else /* MG_VERSION_NUMBER */
     case MG_EV_CONNECT:
       {
       mg_set_timer(m_mgconn, 0);
@@ -112,19 +169,30 @@ void OvmsNetTcpClient::Mongoose(struct mg_connection *nc, int ev, void *ev_data)
       m_netstate = NetConnFailed;
       ConnectionFailed();
       break;
+#endif /* MG_VERSION_NUMBER */
     case MG_EV_CLOSE:
       ESP_LOGD(TAG, "OvmsNetTcpClient Connection closed");
       m_netstate = NetConnDisconnected;
       m_mgconn = NULL;
       ConnectionClosed();
       break;
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+    case MG_EV_READ:
+      {
+      size_t removed = IncomingData(nc->recv.buf, nc->recv.len);
+#else /* MG_VERSION_NUMBER */
     case MG_EV_RECV:
       {
       size_t removed = IncomingData(nc->recv_mbuf.buf, nc->recv_mbuf.len);
+#endif /* MG_VERSION_NUMBER */
       if (removed > 0)
         {
         OvmsMutexLock mg(&m_mgconn_mutex);
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+        mg_iobuf_del(&nc->recv, 0, removed); // Removes `removed` bytes from the beginning of the buffer.
+#else /* MG_VERSION_NUMBER */
         mbuf_remove(&nc->recv_mbuf, removed);
+#endif /* MG_VERSION_NUMBER */
         }
       }
       break;
@@ -133,21 +201,34 @@ void OvmsNetTcpClient::Mongoose(struct mg_connection *nc, int ev, void *ev_data)
     }
   }
 
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+bool OvmsNetTcpClient::Connect(std::string dest, struct mg_tls_opts opts, double timeout)
+#else /* MG_VERSION_NUMBER */
 bool OvmsNetTcpClient::Connect(std::string dest, struct mg_connect_opts opts, double timeout)
+#endif /* MG_VERSION_NUMBER */
   {
   struct mg_mgr* mgr = MyNetManager.GetMongooseMgr();
   if (mgr == NULL) return false;
 
   OvmsMutexLock mg(&m_mgconn_mutex);
   m_dest = dest;
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+  m_timeout = timeout;
+  if ((m_mgconn = mg_connect(mgr, dest.c_str(), OvmsMongooseWrapperCallback, (void *)this)) == NULL)
+#else /* MG_VERSION_NUMBER */
   opts.user_data = this;
   if ((m_mgconn = mg_connect_opt(mgr, dest.c_str(), OvmsMongooseWrapperCallback, opts)) == NULL)
+#endif /* MG_VERSION_NUMBER */
     {
     return false;
     }
   if (timeout > 0.0)
     {
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+    // timeout data
+#else /* MG_VERSION_NUMBER */
     mg_set_timer(m_mgconn, mg_time() + timeout);
+#endif /* MG_VERSION_NUMBER */
     }
   m_netstate = NetConnConnecting;
   return true;
@@ -157,8 +238,13 @@ void OvmsNetTcpClient::Disconnect()
   {
   if (m_mgconn)
     {
+#if MG_VERSION_NUMBER >= MG_VERSION_VAL(7, 0, 0)
+    m_mgconn->fn_data = NULL;
+    m_mgconn->is_closing = 1;         // Tell mongoose to close this connection
+#else /* MG_VERSION_NUMBER */
     m_mgconn->user_data = NULL;
     m_mgconn->flags |= MG_F_CLOSE_IMMEDIATELY;
+#endif /* MG_VERSION_NUMBER */
     m_mgconn = NULL;
     m_netstate = NetConnIdle;
     }
